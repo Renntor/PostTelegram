8e3d13930eb3dbf2f6c3636c633acfe98b3b4cd0
ren <denis30510@gmail.com>
2024-06-09 16:52:20 +0300

    create test for User

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'django_celery_beat',
     'rest_framework_simplejwt',
 
     'users',
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,65 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from users.models import User
 
-# Create your tests here.
+
+class UserTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = User.objects.create(
+            id=500,
+            telegram_id='test',
+            telegram_name='test',
+            is_superuser=True,
+            is_staff=True
+        )
+
+        self.client.force_authenticate(user=self.user)
+
+    def test_destroy_user(self):
+        response = self.client.delete(
+            '/users/500/'
+        )
+
+        self.assertEquals(
+            response.status_code,
+            status.HTTP_204_NO_CONTENT
+        )
+
+    def test_create_user(self):
+        response = self.client.post(
+            '/users/',
+            {'id':501,
+             "telegram_id": 'test_2',
+             "telegram_name": 'test_2',
+             }
+        )
+        self.assertEquals(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+    def test_update_user(self):
+        response = self.client.patch(
+            '/users/500/',
+            {"telegram_id": 'new_test'}
+        )
+        self.assertEquals(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+        self.assertEquals(
+            response.data['telegram_id'],
+            'new_test'
+        )
+
+    def test_get_user(self):
+        response = self.client.get(
+            '/users/500/'
+        )
+
+        self.assertEquals(
+            response.data['telegram_id'],
+            'test'
+        )
